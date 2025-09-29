698584ca7973121cfed854584cd0a5ab713b7818
Birdie-NT <nasrin.membership@gmail.com>
2025-09-29 12:43:08 +0930

    Definition for Binary Tree Node

---
+++ Stack/BinaryTreeInorderTraversal.ts
@@ -0,0 +1,16 @@
+﻿/**
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ */
+
+// Definition for a binary tree node.
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(value?: number, left?: TreeNode | null , right?: TreeNode | null) {
+        this.val = (value === undefined ? 0 : value)
+        this.right = (right === undefined ? null : right)
+        this.left = (left === undefined ? null : left)
+    }
+}
+
